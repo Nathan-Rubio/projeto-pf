38b7f6088c2b48e9384af2eefe53d5f1fc02fdf0
Nathan Rubio <nathan.rubio1908@gmail.com>
2024-08-18 19:49:31 -0300

    modificando main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,69 +1,82 @@
 module Main where
 import System.Random
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.Csv as Csv
-import Data.Vector (Vector)
+
+-- Criação de Tipos para melhor visualização
+type Pesos    = [Double]
+type Vies     = Double
+type Ativacao = Double -> Double -- Função de ativação do neurônio (Neste caso, RELU ou Sigmoid)
 
 -- ADT do Neurônio
 data Neuronio = Neuronio {
-    pesos    :: [Double],
-    vies     :: Double,
-    ativacao :: Double -> Double
+    pesos    :: Pesos,
+    vies     :: Vies,
+    ativacao :: Ativacao
 }
 
 -- Criação do tipo camada, que se trata de uma lista de neurônios
 type Camada = [Neuronio]
 
--- Criação da ADT para uma Rede Neural, formada por camadas, 
--- que se é uma lista de elementos do tipo camada
+-- Criação da ADT para uma Rede Neural, formada por camadas
 newtype RedeNeural = RedeNeural {
     camadas :: [Camada]
 }
- 
--- Criação da ADT para representar a matriz de confusão
+
+-- ADT do Resultado da Matriz de Confusão (Ainda não Implementada)
+data Resultado = TP | FP | TN | FN -- Determina os 4 valores da matriz de confusão
+    deriving (Show, Eq)
+
+-- Função para a Matriz de Confusão (Ainda não Implementada)
 matrizConfusao :: [Double] -> [Double] -> [Resultado]
-matrizConfusao previsoes reais = zipWith classificar
+matrizConfusao previsoes reais = zipWith classificar previsoes reais
     where
         classificar previsao real
-            | previsao >= 0.5 && real == 1.0 = TP
-            | previsao >= 0.5 && real == 0.0 = FP
-            | previsao <  0.5 && real == 0.0 = TN
-            | otherwise                      = FN
-
--- Definição da função sigmoid
-sigmoid :: Double -> Double
-sigmoid z = 1 / (1 + exp (-z))
+            | previsao >= 0.5 && real == 1.0 = TP -- True  Positive
+            | previsao >= 0.5 && real == 0.0 = FP -- False Positive
+            | previsao < 0.5  && real == 0.0 = TN -- True  Negative
+            | otherwise                      = FN -- False Negative
 
 -- Definição da função relu
 relu :: Double -> Double
 relu = max 0
 
+-- Definição da função sigmoid
+sigmoid :: Double -> Double
+sigmoid z = 1 / (1 + exp (-z))
+
+type Inputs = [Double]
+type Output = Double
+
 -- Função responsável por gerar o output do neurônio
-neuronioOutput :: Neuronio -> [Double] -> Double
+-- Multiplica-se o peso com cada input, somando todos e adicionando o viés, obtendo z
+-- z então será utilizado na função de ativação f
+neuronioOutput :: Neuronio -> Inputs -> Output
 neuronioOutput (Neuronio ps v f) inputs =
     let z = sum (zipWith (*) ps inputs) + v
     in f z
 
 -- Função responsável por gerar o output da camada, devolvendo o output de cada neurônio
-camadaOutput :: Camada -> [Double] -> [Double]
+camadaOutput :: Camada -> Inputs -> [Output]
 camadaOutput camada inputs = map (\neuronio -> neuronioOutput neuronio inputs) camada
 
 -- Função responsável por gerar o output da rede neural como um todo
-redeNeuralOutput :: RedeNeural -> [Double] -> [Double]
-redeNeuralOutput (RedeNeural []) inputs = inputs
-redeNeuralOutput (RedeNeural (x:xs)) inputs =
-    let output = camadaOutput x inputs
-    in redeNeuralOutput (RedeNeural xs) output
+redeNeuralOutput :: RedeNeural -> Inputs -> [Output]
+redeNeuralOutput (RedeNeural []) inputs = inputs -- Rede Vazia devolve os inputs
+redeNeuralOutput (RedeNeural (c:cs)) inputs =    -- Passa por cada camada recursivamente, usando o output da anterior
+    let output = camadaOutput c inputs
+    in redeNeuralOutput (RedeNeural cs) output
 
--- Função para inicializar o peso de um neurônio qualquer de maneira randômica
-inicializarPesos :: Int -> Int -> [Double]
+type Seed = Int
+
+-- Função para inicializar os pesos de um neurônio de maneira randômica
+inicializarPesos :: Seed -> Int -> Pesos
 inicializarPesos seed n = take n (randomRs (-1, 1) (mkStdGen seed))
 
--- Função para inicializar um peso randômico em todas as camadas da Rede Neural
-inicializarRedeNeural :: [Int] -> Int -> RedeNeural
+-- Função para inicializar os pesos randômicos em todas as camadas da Rede Neural
+inicializarRedeNeural :: [Int] -> Seed -> RedeNeural
 inicializarRedeNeural estrutura seed =
-    let camadas = zipWith (\n_entrada n_saida -> [Neuronio (inicializarPesos seed n_entrada) 0 relu | _ <- [1..n_saida]]) estrutura (tail estrutura)
-    in RedeNeural camadas
+    let cs = zipWith (\n_entrada n_saida -> [Neuronio (inicializarPesos seed n_entrada) 0 relu | _ <- [1..n_saida]]) (init estrutura) (tail estrutura)
+        ultimaCamada = [Neuronio (inicializarPesos seed (last estrutura)) 0 sigmoid]
+    in RedeNeural (cs ++ [ultimaCamada])
 
 -- Função para o cálculo do Erro Quadrático Médio (Mean Squared Error - MSE)
 mse :: [Double] -> [Double] -> Double
@@ -72,43 +85,84 @@ mse saida desejado = sum [erro ^ 2 | erro <- erros] / fromIntegral (length saida
         erros = [s - d | (s, d) <- zip saida desejado]
 
 -- Função para o cálculo do gradiente simplificado
-gradiente :: Double -> Double
-gradiente saida = saida * 0.01
+gradienteSigmoid :: Double -> Double
+gradienteSigmoid saida = saida * 0.01
 
-atualizarNeuronio :: Neuronio -> [Double] -> [Double] -> Neuronio
-atualizarNeuronio (Neuronio pesos vies ativacao) inputs gradientes =
-    Neuronio novosPesos vies ativacao
-    where
-        -- 0.1 - Taxa de Aprendizagem
-        novosPesos = zipWith(\peso grad -> peso - 0.1 * grad) pesos gradientes
+-- Função para atualizar os pesos do Neurônio
+atualizarNeuronio :: Neuronio -> Inputs -> [Double] -> Neuronio
+atualizarNeuronio (Neuronio ps v f) inputs gradientes =
+    let novosPesos = zipWith (\p (input, grad) -> p - 0.1 * grad * input) ps (zip inputs gradientes)
+        novoVies   = v - 0.1 * sum gradientes
+    in Neuronio novosPesos novoVies f
 
-treinarRedeNeural :: RedeNeural -> [([Double], [Double])] -> Int -> RedeNeural
-treinarRedeNeural rede dataset epocas = foldl treinarEpoca rede [1..epocas]
+type Dataset = [(Inputs, Inputs)]
+type Epocas  = Int
+
+-- Função para o treinamento da Rede Neural
+treinarRedeNeural :: RedeNeural -> Dataset -> Epocas -> IO RedeNeural
+treinarRedeNeural rede dataset epocas = treinar rede epocas
     where
-        treinarEpoca redeAtual _ = foldl (\rede (inputs, desejado) ->
-            let saida      = redeNeuralOutput redeAtual inputs
-                erro       = mse saida desejado
-                gradientes = map gradiente saida
-                novaRede   = atualizarPesos redeAtual inputs gradientes
-            in novaRede) redeAtual dataset
+        treinar redeAtual 0 = return redeAtual -- Se não tiver mais épocas, retorna a rede atual
+        treinar redeAtual n = do
+            let novaRede = foldl (\rede (inputs, desejado) ->
+                    let saida      = redeNeuralOutput rede inputs
+                        erro       = mse saida desejado
+                        gradientes = map gradienteSigmoid saida
+                        novaRede   = atualizarPesos rede inputs gradientes
+                    in novaRede) redeAtual dataset
+                -- Monitorando o erro e pesos após cada época
+                erroTotal = sum [mse (redeNeuralOutput novaRede inputs) desejado | (inputs, desejado) <- dataset] / fromIntegral (length dataset)
+            putStrLn $ "Época: " ++ show (epocas - n + 1) ++ ", Erro Total: " ++ show erroTotal
+            mapM_ printCamada (camadas novaRede)
+            putStrLn ""
+            treinar novaRede (n - 1)
 
 -- Função para atualizar a rede com novos pesos
-atualizarPesos :: RedeNeural -> [Double] -> [Double] -> RedeNeural
-atualizarPesos (RedeNeural camadas) inputs gradientes =
-    RedeNeural (map (\camada -> map (\neuronio -> atualizarNeuronio neuronio inputs gradientes) camada) camadas)
+atualizarPesos :: RedeNeural -> Inputs -> [Double] -> RedeNeural
+atualizarPesos (RedeNeural cs) inputs gradientes =
+    RedeNeural (map (\c -> map (\neuronio -> atualizarNeuronio neuronio inputs gradientes) c) cs)
 
-redeExemplo :: RedeNeural
-redeExemplo = RedeNeural [
-    [Neuronio [0.5, -0.2] 0.1 relu, Neuronio [0.3, 0.8] (-0.4) relu], --Primeira Camada
-    [Neuronio [0.7,  0.3] 0.2 sigmoid]                                -- Camada de Saída
-    ]
+-- Função para imprimir os pesos de uma camada
+printCamada :: Camada -> IO ()
+printCamada camada = do
+    mapM_ printNeuronio camada
+    putStrLn ""
 
+-- Função para imprimir os pesos de um neurônio
+printNeuronio :: Neuronio -> IO ()
+printNeuronio (Neuronio ps v _) = putStrLn $ "Pesos: " ++ show ps ++ ", Vies: " ++ show v
 
 main :: IO ()
 main = do
-    let rede         = inicializarRedeNeural [2, 2, 1] 42
-    let dataset      = [([0.5, -0.3], [1.0]), ([0.1, 0.8], [0.0])]
-    let redeTreinada = treinarRedeNeural rede dataset 1000
-    let resultado    = redeNeuralOutput redeTreinada [0.9, -0.1]
+    -- Conjunto de dados de treinamento normalizados
+    let datasetTreinamento = [([0.444, 0.500], [0.0]), 
+                              ([0.556, 0.625], [1.0]), 
+                              ([0.333, 0.125], [0.0]),
+                              ([0.878, 0.825], [1.0]),
+                              ([0.100, 0.228], [0.0]),
+                              ([0.738, 0.611], [1.0]),
+                              ([0.456, 0.222], [0.0]),
+                              ([0.667, 0.773], [1.0])
+                             ]
 
-    putStrLn $ "Resultado da Rede Neural Treinada: " ++ show resultado
+    -- Inicia a rede neural com 3 camadas
+    -- 2 neurônios - 1° camada
+    -- 2 neurônios - 2° camada
+    -- 1 neurônio  - 3° camada (saída)
+    -- 42 - Semente usada para esta Rede
+    let rede = inicializarRedeNeural [2, 2, 1] 42
+    putStrLn "Pesos Iniciais: "
+    mapM_ printCamada (camadas rede)
+
+    -- Obtém-se a rede neural treinada
+    -- Usa-se o datasetTreinamento em 10 épocas
+    redeTreinada <- treinarRedeNeural rede datasetTreinamento 5
+
+    -- Resultados de testes básicos
+    let resultado1 = redeNeuralOutput redeTreinada [0.945, 0.832]
+    let resultado2 = redeNeuralOutput redeTreinada [0.211, 0.102]
+    let resultado3 = redeNeuralOutput redeTreinada [0.811, 0.418]
+
+    putStrLn $ "Resultado 1: " ++ show resultado1
+    putStrLn $ "Resultado 2: " ++ show resultado2
+    putStrLn $ "Resultado 3: " ++ show resultado3
